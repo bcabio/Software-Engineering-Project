d9838592d87243acc94b8c2a0b308e6142988daa
bcabio <briancabigon@gmail.com>
2018-04-08 02:43:03 -0400

    Testing deployment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 // require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
+unregister();
